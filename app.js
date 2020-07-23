2b76ff80e50acd67b76ef986ca65a898d4a0f16c
fishwickid <chadfishwick@gmail.com>
2020-07-24 07:21:17 +0800

    added routes

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const fs = require("fs");
+// Connecting Dependencies
 
-const PORT = process.env.PORT || 3000;
+const express = require('express');
+const path = require('path');
+const app = express();
+const fs = require('fs');
+const noteData = require('./db/db.json');
+
+
+// Connecting server to Herouko or PORT 3000 as a default
+let PORT = process.env.PORT || 3000;
+
+// Middleware require for the app, these are additionaly function methods for Express
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-const apiRoutes = require("./routes/api");
-const htmlRoutes = require("../routes/htmljs");
+// Create routes the the HTML Pages using GET requests
 
-app.use(apiRoutes);
-app.use(htmlRoutes);
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
 
-app.listen(PORT, function(){
-    console.log("App listening on PORT: " + PORT);
-});
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(noteData);
+})
+
+app.post('/api/notes', (req, res) => {
+    noteData.push(req.body);
+    noteData.forEach((note, i) => {
+        note.id = i + 1;
+    })
+    let newNote = JSON.stringify(noteData);
+    fs.writeFileSync('./db/db.json', newNote);
+
+    res.json(noteData);
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+
+    let filtered = noteData.filter(note => note.id !== parseInt(req.params.id));
+    fs.writeFileSync('./db/db.json', JSON.stringify(filtered));
+
+    //alter the note data with the filtered results so when it sends the response back it is immediately showned on the front end
+    noteData = filtered;
+
+    res.json(noteData);
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+})
